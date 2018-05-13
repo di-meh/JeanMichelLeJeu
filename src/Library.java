69f90d21b8bad001a8eec6c5bed81311aa642bb8
tbouzidi <38916179+tbouzidi@users.noreply.github.com>
2018-05-13 12:59:04 +0200

    ce que doit contenir la bibliotheque

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,10 @@
 
 public class Library {
 
-
+	/*ça contient toutes tes images
+	et tes données genre dimensions du personnage
+	des constantes genre les pv des ennemis et tout
+	comme ça quand tu change les pv tu as juste à 
+	changer la valeur dans la bibliothèque*/
 	
 }
