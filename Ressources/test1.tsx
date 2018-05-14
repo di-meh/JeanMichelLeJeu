ff140478028769e952d776eda4b388170e597719
TANINA BOUZIDI <tbouzidi@iut.univ-paris8.fr>
2018-05-14 13:45:04 +0200

    mes fichiers de recherches/test

---
+++ Ressources/test1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test1" tilewidth="32" tileheight="32" tilecount="170" columns="17">
+ <image source="../../../Téléchargements/Base.png" width="568" height="329"/>
+</tileset>
