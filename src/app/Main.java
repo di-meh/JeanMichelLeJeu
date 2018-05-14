f26314f15ad73928f56cb4009853384749e397ad
MEHDI SABER <msaber@iut.univ-paris8.fr>
2018-05-14 16:21:21 +0200

    Ajout d'un TODO

--- src/app/Main.java
+++ src/app/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception { //TODO Faire en sorte que la boucle fonctionne et que le modèle soit fonctionnel.
 
 		try {
 
