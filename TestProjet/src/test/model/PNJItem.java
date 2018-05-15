d5cce7f630348fad0c093c92b47911e0e439abcc
TANINA BOUZIDI <tbouzidi@iut.univ-paris8.fr>
2018-05-15 16:23:30 +0200

    creation objet pnj, item et ennemi

---
+++ TestProjet/src/test/model/PNJItem.java
@@ -0,0 +1,12 @@
+
+public class PNJItem  extends PNJ{
+
+	public PNJItem(int x, int y) {
+		super("", "", x, y);
+	}
+
+	public String parler() {
+		return "Hey ! T'as entendu parler de ce truc ? le "; //inserer objet region
+	}
+	
+}
