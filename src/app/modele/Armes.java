142d5c613a6e4d9c608f4fe6138b37a3d76d33b1
FLORIAN PASQUIER <fpasquier@iut.univ-paris8.fr>
2018-05-15 15:06:31 +0200

    ajout de la classe "Armes"

---
+++ src/app/modele/Armes.java
@@ -0,0 +1,32 @@
+package app.modele;
+
+public class Armes {
+	private String nom;
+	private String zoneAdapt;
+	private int dgtZone;
+	private int dgtPasZone;
+	
+	public Armes(String n, String za, int dgtPZ, int dgtZ) {
+		nom=n;
+		zoneAdapt=za;
+		dgtPasZone=dgtPZ;
+		dgtZone=dgtZ;
+}
+
+	public final String getNom() {
+		return nom;
+	}
+
+	public final String getZoneAdapt() {
+		return zoneAdapt;
+	}
+
+	public final int getDgtZone() {
+		return dgtZone;
+	}
+
+	public final int getDgtPasZone() {
+		return dgtPasZone;
+	}
+
+}
