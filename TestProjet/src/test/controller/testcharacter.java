6f667b5347d7502f271223ebce98cd22b75e5591
FLORIAN PASQUIER <fpasquier@iut.univ-paris8.fr>
2018-05-16 11:10:33 +0200

    TEST

---
+++ TestProjet/src/test/controller/testcharacter.java
@@ -0,0 +1,25 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+public class testcharacter implements Initializable{
+	@FXML
+	private TilePane tilepane;
+	@FXML
+	private ImageView image;
+	
+	public testcharacter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
