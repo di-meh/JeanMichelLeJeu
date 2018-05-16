e839b273304ee79fa7dd2ae5f9d837453dfa07ba
FLORIAN PASQUIER <fpasquier@iut.univ-paris8.fr>
2018-05-16 11:20:04 +0200

    test character

--- src/app/controleur/testcharacter.java
+++ src/app/controleur/testcharacter.java
@@ -1,6 +1,7 @@
+package app.controleur;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
@@ -18,8 +19,8 @@ public class testcharacter implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
+
 
 }
