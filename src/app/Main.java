5d8978c10116b05238fbfa5536e6d97476273c14
FLORIAN PASQUIER <fpasquier@iut.univ-paris8.fr>
2018-05-17 10:43:06 +0200

    Essai deplacement

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,10 +2,11 @@ package app;
 
 import java.io.File;
 import java.net.URL;
-
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,9 +18,8 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
 		try {
-
+			primaryStage.setTitle("Jean-Michel");
 			FXMLLoader loader = new FXMLLoader();
 			URL url = new File("src/app/vue/Map.fxml").toURI().toURL();
 			loader.setLocation(url);
@@ -28,9 +28,12 @@ public class Main extends Application {
 			Scene scene = new Scene(root, root.getPrefHeight(),root.getPrefWidth());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 
 	}
 }
--- src/app/controleur/Controleur.java
+++ src/app/controleur/Controleur.java
@@ -5,15 +5,18 @@ import java.util.ResourceBundle;
 
 import app.modele.Jeu;
 import app.modele.Map;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 
 
-public class Controleur implements Initializable{
+public class Controleur implements Initializable, EventHandler<KeyEvent>{
 
 	private Jeu jeu;
 	private Map map;
@@ -25,11 +28,13 @@ public class Controleur implements Initializable{
 	private TilePane tilemap;
 
 	private Image imagetest;
-    
+
+
+
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		String nomImage;
-
 		this.tilemap.setPrefColumns(12);
 		this.tilemap.setPrefRows(12);
 		this.map = new Map();
@@ -44,11 +49,13 @@ public class Controleur implements Initializable{
 				this.tilemap.getChildren().add(img);
 			}
 		}
-
 		this.pane.getChildren().add(new ImageView(new Image("./app/img/Female_Sprite_face2.png")));
 		pane.getChildren().get(1).setLayoutX(84);
 		pane.getChildren().get(1).setLayoutY(84);
-
+		pane.getChildren().get(1).setOnKeyTyped(e -> {
+			if(e.getCode() == KeyCode.Z)
+			pane.getChildren().get(1).setLayoutX(pane.getChildren().get(1).getLayoutX()+16);
+			});
 	}
 
 
@@ -58,9 +65,16 @@ public class Controleur implements Initializable{
 	}
 
 
-
 	public void setJeu(Jeu jeu) {
 		this.jeu = jeu;
+	}
+
+
+
+	@Override
+	public void handle(KeyEvent event) {
+		// TODO Auto-generated method stub
+		pane.getChildren().get(1).setLayoutX(pane.getChildren().get(1).getLayoutX()+12);
 	}
 
 }
