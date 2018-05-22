e2d5aa2460d7cdceaf4b631525861db73269693a
MEHDI SABER <msaber@iut.univ-paris8.fr>
2018-05-22 14:41:31 +0200

    création de getters et setters pour le bind

--- src/app/vue/VueJeanMichel.java
+++ src/app/vue/VueJeanMichel.java
@@ -1,5 +1,25 @@
 package app.vue;
 
-public class VueJeanMichel {
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
+public class VueJeanMichel {
+	
+public void gererfleche(KeyEvent e) {
+		
+		KeyCode value = e.getCode();
+		switch (value) {
+		case Z : pane.getChildren().get(1).setLayoutY(pane.getChildren().get(1).getLayoutY()-5);
+			//TODO appeler une méthode sur le personnage qui déplace le y
+			break;
+		case Q : pane.getChildren().get(1).setLayoutX(pane.getChildren().get(1).getLayoutX()-5);
+			break;
+		case S : pane.getChildren().get(1).setLayoutY(pane.getChildren().get(1).getLayoutY()+5);
+			break;
+		case D : pane.getChildren().get(1).setLayoutX(pane.getChildren().get(1).getLayoutX()+5);
+			break;
+		default:
+			break;
+		}
+	}
 }
