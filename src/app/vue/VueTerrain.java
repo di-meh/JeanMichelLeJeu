d92095aae1d72ced37a8d6278236a16f9506b416
MEHDI SABER <msaber@iut.univ-paris8.fr>
2018-05-22 15:30:30 +0200

    Débuts des implémentations du MVC

--- src/app/vue/VueTerrain.java
+++ src/app/vue/VueTerrain.java
@@ -1,5 +1,13 @@
 package app.vue;
 
-public class VueTerrain {
+import app.modele.Terrain;
 
+public class VueTerrain {
+	
+	private Terrain terrain;
+	
+	public VueTerrain(Terrain t) {
+		this.terrain = t;
+	}
+	
 }
