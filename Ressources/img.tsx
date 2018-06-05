021fdfc741c8f183adf284b0b8c89935d32d4a94
MEHDI SABER <msaber@iut.univ-paris8.fr>
2018-06-05 09:35:50 +0200

    Nettoyage du dossier Ressources

--- Ressources/img.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="img" tilewidth="16" tileheight="16" tilecount="350" columns="25">
- <image source="tiles.png" width="400" height="224"/>
-</tileset>
--- Ressources/test1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="test1" tilewidth="32" tileheight="32" tilecount="170" columns="17">
- <image source="../../../Téléchargements/Base.png" width="568" height="329"/>
-</tileset>
